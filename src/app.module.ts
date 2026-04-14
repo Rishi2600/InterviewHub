7cf65810f875f998e3e1c5732bb46764863f3715
Rishi2600 <rishiraj6177@gmail.com>
2026-04-14 14:05:11 +0530

    chores: chat init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, RoomsModule],
+  imports: [AuthModule, UsersModule, RoomsModule, ChatModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
