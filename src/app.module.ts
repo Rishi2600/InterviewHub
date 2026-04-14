19477286bf5d4769301537d2a37c950cd63ef2a8
Rishi2600 <rishiraj6177@gmail.com>
2026-04-14 13:56:52 +0530

    chores: rooms init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, RoomsModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
