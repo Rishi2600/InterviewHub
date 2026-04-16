25c6b9c5d6b34d227bd1203cfa5c0fc4e75ecda9
Rishi2600 <rishiraj6177@gmail.com>
2026-04-16 16:43:51 +0530

    cleanup and app module wiring changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
+
 import { AuthModule } from './auth/auth.module';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { ChatModule } from './chat/chat.module';
@@ -10,8 +11,22 @@ import { ContestsModule } from './contests/contests.module';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, RoomsModule, ChatModule, ContestsModule, LeaderboardModule],
-  controllers: [AppController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ThrottlerModule.forRoot([{ ttl: 1000, limit: 5 }]),
+    AuthModule,
+    UsersModule,
+    RoomsModule,
+    ChatModule,
+    ContestsModule,
+    LeaderboardModule,
+  ],
 })
 export class AppModule {}
